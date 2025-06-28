99a1fa54a61127da35f184bd5236212796ec1045
Mbali Sithebe <2567890@students.wits.ac.za>
2025-06-28 12:14:07 +0200

    updates

--- src/app.js
+++ src/app.js
@@ -2,12 +2,44 @@
 function refreshWeather(response) {
   let temparatureElement = document.querySelector("#temparature");
   let temperature = response.data.temperature.current;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
 
   //Changing the round off
   temparatureElement.innerHTML = Math.round(temperature);
+  // Weather Description
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+//Formating the Date
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 //API Search City
@@ -29,6 +61,6 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 //Default City
-searchCity("Johannesburg")
+searchCity("Johannesburg");
 
 //API Intergration
