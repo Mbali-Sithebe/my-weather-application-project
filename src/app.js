534c839e0b407e7ec11cfbdee46cdf144d4ac92c
Mbali Sithebe <2567890@students.wits.ac.za>
2025-06-28 10:58:49 +0200

    updates

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
+//Change Weather Temparature
+function refreshWeather(response) {
+  let temparatureElement = document.querySelector("#temparature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  //Changing the round off
+  temparatureElement.innerHTML = Math.round(temperature);
+}
+
+//API Search City
+function searchCity(city) {
+  let apiKey = "f4fo17tb49d11f665b0eb0019e73da1a";
+  //Difference between API KEY and URL
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 //Search Form Function
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-}
 
+  searchCity(searchInput.value);
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+//Default City
+searchCity("Johannesburg")
 
 //API Intergration
